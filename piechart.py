334abebef49760a0ccec8de1a6b96bf726214050
Cassandra Talbot <40823509+CassandraTalbot32@users.noreply.github.com>
2022-10-20 12:56:49 +0100

    Update piechart.py

--- piechart.py
+++ piechart.py
@@ -2,8 +2,10 @@ import pandas as pd
 from matplotlib.pyplot import pie, axis, show
 %matplotlib inline
 
+# Read file 
 df = pd.read_csv ('chart_work.csv')
 
+# Calculate percentages to use in pie chart
 sums = df.groupby(df["Product Name;"])["Number Of Bugs"].sum()
 axis('equal');
 pie(sums, labels=sums.index);
